4e3615cc7f1171319966f56ba6ba069c794d7a3f
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2025-03-21 18:58:16 +0200

    Home view implemented

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3001;
 
 let desert = require('./data/env_01.json');
 let rainforest = require('./data/env_02.json');
 let tundra = require('./data/env_03.json');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home/home.html'));
+});
 
 app.get('/1', (req, res) => {
     res.send(desert);
